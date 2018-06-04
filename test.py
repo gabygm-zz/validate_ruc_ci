8b620c748ff803838c9ffda9542207eb0cdfa1b5
Gaby G <paogaby0803@gmail.com>
2018-06-03 21:29:14 -0500

    test ci/ruc

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ class TestIdentifer(unittest.TestCase):
 
     def test_rules_ci(self):
         self.assertEqual(Identifier.rules_identifier('9999999999'), False)
+        self.assertEqual(Identifier.rules_identifier('1105137143'), 0)
+        self.assertEqual(Identifier.rules_identifier('1105137143'),0)
+
+    def test_validate_ci(self):
+        self.assertFalse(Identifier.validate_ci_ruc('28321321312'))
+        self.assertEqual(Identifier.validate_ci_ruc('1105137143'), True)
+        self.assertEqual(Identifier.validate_ci_ruc('1105137143001'), True)
+
 
 if __name__ == '__main__':
     unittest.main()
